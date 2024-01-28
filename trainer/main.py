80fcecb446bec7a357f103a95ff1e89e46ab3e39
Martin Novák <martin.novak@gmx.com>
2024-01-28 22:05:40 +0100

    Add data sample

--- trainer/main.py
+++ trainer/main.py
@@ -10,6 +10,9 @@ from train import train
 
 
 def main():
+    if not os.path.exists("network"):
+        os.makedirs("network")
+
     def load_config(config_path="config.json"):
         with open(config_path, 'r') as config_file:
             return json.load(config_file)
