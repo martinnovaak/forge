cc221fbad5f8e71db221ea120b7e11b65c1f2d20
Martin Novák <novak213@cvut.cz>
2024-05-05 21:35:05 +0200

    Add training resuming

--- trainer/train.py
+++ trainer/train.py
@@ -7,27 +7,50 @@ from quantize import quantize
 
 def print_epoch_stats(epoch, running_loss, iterations, fens, start_time, current_time):
     epoch_time = current_time - start_time
-    message = ("epoch {:<2} | time: {:.2f} s | epoch loss: {:.4f} | speed: {:.2f} pos/s"
+    message = ("\nepoch {:<2} | time: {:.2f} s | epoch loss: {:.4f} | speed: {:.2f} pos/s"
                .format(epoch, epoch_time, running_loss.item() / iterations, fens / epoch_time))
     print(message)
 
-def train(model: torch.nn.Module, optimizer: torch.optim.Optimizer, dataloader: BatchLoader, epochs: int, lr_drop_steps: int, device: torch.device):
+def save_checkpoint(model, optimizer, epoch, loss, filename):
+    checkpoint = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss,
+    }
+    torch.save(checkpoint, filename)
+
+def load_checkpoint(model, optimizer, filename, resume_training=False):
+    checkpoint = torch.load(filename)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    loss = checkpoint['loss']
+    return model, optimizer, epoch, loss
+
+def train(model: torch.nn.Module, optimizer: torch.optim.Optimizer, dataloader: BatchLoader, epochs: int, lr_drop_steps: int, device: torch.device, resume_training: bool = False):
+    if resume_training:
+        model, optimizer, start_epoch, best_loss = load_checkpoint(model, optimizer, "checkpoint.pth")
+    else:
+        start_epoch = 0
+
     running_loss = torch.zeros(1, device=device)
     epoch_start_time = time()
     iterations = 0
     fens = 0
-    epoch = 0
+    epoch = start_epoch
 
     while epoch < epochs:
         new_epoch, batch = dataloader.next_batch(device)
         if new_epoch:
             epoch += 1
 
-            if epoch % lr_drop_steps == 0:
-                optimizer.param_groups[0]["lr"] *= 0.1
-
             current_time = time()
             print_epoch_stats(epoch, running_loss, iterations, fens, epoch_start_time, current_time)
+
+            if epoch % lr_drop_steps == 0:
+                optimizer.param_groups[0]["lr"] *= 0.1
+                print("LR dropped")
 
             running_loss = torch.zeros(1, device=device)
             epoch_start_time = current_time
@@ -47,3 +70,6 @@ def train(model: torch.nn.Module, optimizer: torch.optim.Optimizer, dataloader: 
         running_loss += loss
         iterations += 1
         fens += batch.size
+
+        if fens % 163_840 == 0:
+            print("\rTotal fens parsed in this epoch:", fens, end='', flush=True)
